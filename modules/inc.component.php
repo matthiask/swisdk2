a76f3dc700a814aabc53712b67e8b84ad08ee222
Moritz Zumbuehl <mail@momoetomo.ch>
2006-04-23 21:16:47 +0200

    ???

--- modules/inc.component.php
+++ modules/inc.component.php
@@ -5,12 +5,84 @@
 	*	Read the entire license text here: http://www.gnu.org/licenses/gpl.html
 	*/
 
+
+
+	class BasicConfigurationComponent
+	{
+		protected $mConfiguration = array();
+		
+		/**
+		*	Returns a copy of the configuration array.
+		*/
+		public function getConfiguration() {
+			return $this->mConfiguration;
+		}
+		
+		/**
+		*	Returns the configuration value for the $name and if no key/value pair is found null.
+		*	(the key is case insensitive!)
+		*/
+		public function getConfigValue( $name , $default = null ) {
+			
+			$name = strtolower( $name );
+			if( isset( $this->mConfiguration[ $name ] ) ) {
+				return $this->mConfiguration[ $name ];
+			}
+			
+			return $default;
+		}
+		
+		/**
+		*	Sets the config value for the $name parameter. If the key is allready present the value is overwritten, but only
+		*	if $overwrite is true.
+		*	(the key is case insensitive!)
+		*/
+		public function setConfigValue( $name , $value , $overwrite = true ) {
+		
+			$name = strtolower( $name );
+			if( !$overwrite && isset( $this->mConfiguration[ $name ] ) ) {
+				return false;
+			}
+			
+			return $this->mConfiguration[ $name ] = $value;
+		}
+		
+		/**
+		*	Merges the current config values with the new.
+		*/
+		public function setConfigValues( $values ) {
+			
+			if( is_array( $values ) ) {
+				$values = array_change_key_case( $values , CASE_LOWER );
+				$this->mConfiguration = array_merge( $this->mConfiguration , $values);
+			}
+		}
+		
+		/**
+		*	Deletes all config values.
+		*/
+		public function emptyConfig() {
+			$this->mConfiguration = array();	
+		}
+		
+		public function __get( $var )
+		{
+			return $this->getConfigValue( $var );
+		}
+
+		public function __set($var, $value)
+		{
+			$this->setConfigValue( $var , $value );
+		}
+	}
+
 	interface IComponent {
 		public function run();
 	}
 
 	interface IHtmlComponent extends IComponent {
 		public function html();
+		public function name();
 	}
 
 	interface IFeedComponent extends IComponent {
@@ -61,6 +133,38 @@
 		public function set_smarty_variables(&$smarty);
 	}
 
-
+	
+	class BasicViewComponent implements IHtmlComponent
+	{
+		protected $mSmarty = null;
+		protected $mTemplate = "";
+		
+		public function __construct( $template , $data , $name = "data" )
+		{
+			require_once SWISDK_ROOT . "modules/inc.smarty.php";
+			
+			$this->mSmarty = new SwisdkSmarty();
+			$this->mSmarty->assign( $name , $data );
+			$this->mTemplate = $template;
+			
+			
+		}
+		
+		public function run()
+		{
+			return true;
+		}
+		
+		public function name()
+		{
+			return "content";
+		}
+		
+		public function html()
+		{
+			return $this->mSmarty->fetch( $this->mTemplate );
+		}
+	
+	}
 
 ?>
