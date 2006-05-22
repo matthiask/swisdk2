a6ad0438fcbf7d471dd1fab14f4a1088529dfb66
Matthias Kestenholz <matthias@spinlock.ch>
2006-05-22 21:55:59 +0200

    adapt smarty module to coding style

--- modules/inc.smarty.php
+++ modules/inc.smarty.php
@@ -9,7 +9,7 @@
 	define( 'STREGION_FULL' , 1 );
 	define( 'STREGION_HEADER' , 2 );
 	define( 'STREGION_FOOTER' , 3 );
-	
+
 	/**
 	 * E_STRICT wrapper for PHP4 compatible Smarty code!
 	 */
@@ -39,7 +39,7 @@
 			error_reporting($er);
 			return $ret;
 		}
-		
+	
 		public function __get($var)
 		{
 			$er = error_reporting(E_ALL^E_NOTICE);
@@ -63,44 +63,44 @@
 		private static $mInstance = null;
 		private $mSmarty = null;
 		private $mHtmlHandlers = array();
-		
-		/** 
+	
+		/**
 		*	I'm a singleton!
 		*/
 		private function __construct() {}
-		
+	
 		/**
 		*	Use this to get the instance of the SmartyMaster.
 		*	This method calls by the frst time also setup().
 		*	So you don't have to call setup() after retrieving the instance.
 		*/
 		public static function instance() {
-		
+	
 			if( SmartyMaster::$mInstance === null ) {
 				SmartyMaster::$mInstance = new SmartyMaster();
 				SmartyMaster::$mInstance->setup();
 			}
-			
+		
 			return SmartyMaster::$mInstance;
 		}
-	
+
 		/**
 		*	Setups the smarty master. The smarty master needs following config values:
 		*	To know in which config section the template paths are stored the master reads
-		*	the "runtime.website" value. This value is typically written by the 
+		*	the "runtime.website" value. This value is typically written by the
 		*	dispatcher.
 		*/
-		public function setup( $website = null ) 
+		public function setup( $website = null )
 		{
-			
+		
 			// the website ... we need that lather to read out the correct section
 			// in the config
 			if( $website === null ) {
 				$website = Swisdk::config_value( "runtime.website" );
-			}	
-			
+			}
+		
 			$this->mWebsite = $website;
-			
+		
 			// check if there is a config section with values
 			$cf = $this->mWebsite . ".";
 			$this->mFullTemplate = Swisdk::config_value( $cf . "fullTemplate" );
@@ -108,34 +108,31 @@
 			$this->mFooterTemplate = Swisdk::config_value( $cf . "footer" );
 			$this->mTitle = Swisdk::config_value( $cf . "title" );
 		}
-	
+
 		/**
 		*	Returns the reference to the smarty object of type SwisdkSmarty.
 		*/
-		public function getSmartyRef() 
+		public function smarty()
 		{
-			if( $this->mSmarty === null ) 
-			{
+			if( $this->mSmarty === null ) {
 				$this->mSmarty = new SwisdkSmarty();
-			}	
+			}
 			return $this->mSmarty;
 		}
-		
+	
 		/**
-		*	Display a template. 
+		*	Display a template.
 		*/
-		public function display( $template = "" , $region = STREGION_FULL ) 
+		public function display( $template = "" , $region = STREGION_FULL )
 		{
-			if( $template === "" )	
-			{
+			if( $template === "" ) {
 				$template = $this->mFullTemplate;
 			}
-			
-			$smarty = $this->getSmartyRef();
+		
+			$smarty = $this->smarty();
 			if( $smarty->template_exists( $template ) ) {
-				
-				if( $this->generateHandlerOutput( $region ) )
-				{
+			
+				if( $this->generate_handler_output( $region ) ) {
 					$smarty->display($template);
 					return true;
 				} else  {
@@ -143,75 +140,71 @@
 					SwisdkError::handle( new FatalError( "" ) );
 					return false;
 				}
-				
+			
 			} else {
 				// FIXME handle proper error... ressource not found error
 				SwisdkError::handle( new FatalError("Template not found! Path: $template") );
-				return false;				
+				return false;			
 			}
 		}
-		
-		public function displayHeader( $region = STREGION_HEADER ) 
+	
+		public function display_header( $region = STREGION_HEADER )
 		{
 			return $this->display( $this->mHeaderTemplate , $region );
 		}
-		
-		public function displayFooter( $region = STREGION_FOOTER  )
+	
+		public function display_footer( $region = STREGION_FOOTER  )
 		{
 			return $this->display( $this->mFooterTemplate , $region );
 		}
-		
-		public function addHtmlHandler( IHtmlComponent $component , $region = STREGION_FULL )
+	
+		public function add_html_handler( IHtmlComponent $component , $region = STREGION_FULL )
 		{
-			if( !is_array($this->mHtmlHandlers[ $region ] ) )
-			{
+			if( !is_array($this->mHtmlHandlers[ $region ] ) ) {
 				$this->mHtmlHandlers[ $region ] = array( $component );
 			} else {
 				$this->mHtmlHandlers[ $region ][] = $component;
-			}			
+			}		
 		}
-		
-		public function generateHandlerOutput( $region = STREGION_ALL )
-		{			
+	
+		public function generate_handler_output( $region = STREGION_ALL )
+		{		
 			if( $region === STREGION_ALL ) {
-				
+			
 				foreach( $this->mHtmlHandlers as $hGroup )
 				{
-					if( is_array( $hGroup ) ) 
-					{
-						if( !$this->makeHandlerOutput( $hGroup ) )
-						{
+					if( is_array( $hGroup ) ) {
+						if( !$this->make_handler_output( $hGroup ) ) {
 							return false;
 						}
 					}
 				}
-				
+			
 			} else {
 
-				if( is_array( $this->mHtmlHandlers[$region] ) ) 
-					return $this->makeHandlerOutput( $this->mHtmlHandlers[$region] );
+				if( is_array( $this->mHtmlHandlers[$region] ) )
+					return $this->make_handler_output( $this->mHtmlHandlers[$region] );
 			}
-			
+		
 			return true;
 		}
-		
-		
-		public function makeHandlerOutput( $hGroup )
+	
+	
+		public function make_handler_output( $hGroup )
 		{
 			foreach( $hGroup as $element )
 			{
-				$smarty = $this->getSmartyRef();
-				
+				$smarty = $this->smarty();
+			
 				$content = $element->html();
-				if( SwisdkError::isError($content) ) 
-				{	
+				if( SwisdkError::is_error($content) ) {
 					// FIXME return the error up to were generation error is outputed
 					return false;
 				}
-				
+			
 				$smarty->assign( $element->name() , $content );
 			}
-			
+		
 			return true;
 		}
 	}
