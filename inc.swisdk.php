9549457f03a144e4a3eb7517060b75882231618f
Matthias Kestenholz <matthias@spinlock.ch>
2006-05-30 21:37:02 +0200

    Cleanup inc.swisdk.php
    Signed-off-by: Matthias Kestenholz <matthias@spinlock.ch>

--- inc.swisdk.php
+++ inc.swisdk.php
@@ -7,62 +7,41 @@
 	*	Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
 	*/
 
-	date_default_timezone_set('Europe/Zurich');
-
 	class Swisdk {
-	
+
 		public static function runFromHttpRequest()
 		{
 			define('APP_ROOT', $_SERVER['DOCUMENT_ROOT'] . '/../../');
-			define('HTDOCS_ROOT', APP_ROOT . 'webapp/htdocs/');
-			define('SWISDK_ROOT', APP_ROOT . 'swisdk/');
-			define('SMARTY_ROOT', SWISDK_ROOT . 'lib/smarty/');
-			define('MODULE_ROOT', SWISDK_ROOT . 'modules/');
-			define('WEBAPP_ROOT', APP_ROOT.'webapp/');
-			define('LOG_ROOT', APP_ROOT.'log/');
-			define('CONTENT_ROOT' , WEBAPP_ROOT . 'content/');
-			define('CACHE_ROOT', WEBAPP_ROOT.'cache/');
-			
-			require_once SWISDK_ROOT . 'core/inc.functions.php';
-			require_once SWISDK_ROOT . 'core/inc.error.php';
-		
+
 			Swisdk::run(array('REQUEST_URI' =>
-				((isset($_SERVER['HTTPS'])&&$_SERVER['HTTPS']=='on')?'https://':'http://')
+				((isset($_SERVER['HTTPS'])&&$_SERVER['HTTPS']=='on')
+					?'https://':'http://')
 				.$_SERVER['SERVER_NAME']
 				.(($_SERVER['SERVER_PORT']!=80)?':'.$_SERVER['SERVER_PORT']:'')
 				.$_SERVER['REQUEST_URI']));
 		}
-	
+
 		public static function runFromCommandLine()
 		{
-		
 			/**
-			 * the $_Server[Document_root] is not set when the call comes from the commandline.
-			 * so we use the SCRIPT_NAME and assume that the file is in APP_ROOT/swisdk/commandline.php
+			 * the $_Server[Document_root] is not set when the call comes from the
+			 * commandline.
+			 * so we use the SCRIPT_NAME and assume that the file is in
+			 * APP_ROOT/swisdk/commandline.php
 			 */
 			$swisdk = substr( __FILE__ ,  0 , strrpos(__FILE__ , "/") );
 			$apppath = substr( $swisdk , 0 ,  strrpos( $swisdk , "/")+1 );
 
 			define( 'APP_ROOT', $apppath );
-			define( 'HTDOCS_ROOT', APP_ROOT . 'webapp/htdocs/' );
-			define( 'SWISDK_ROOT', APP_ROOT . 'swisdk/' );
-			define( 'SMARTY_ROOT', SWISDK_ROOT . 'lib/smarty/' );
-			define( 'MODULE_ROOT', SWISDK_ROOT . 'modules/' );
-			define( 'CONTENT_ROOT' , APP_ROOT . 'webapp/content/' );
-			define('LOG_ROOT', APP_ROOT.'log/');
-			define('CACHE_ROOT', WEBAPP_ROOT.'cache/');
-			
-			require_once SWISDK_ROOT . 'core/inc.functions.php';
-			require_once SWISDK_ROOT . 'core/inc.error.php';
-		
+
 			$requestUri = '';
 			if( isset( $_SERVER['argv'][1]) ) {
 				$requestUri = $_SERVER['argv'][1];
 			}
-		
+
 			Swisdk::run( array( 'REQUEST_URI' => $requestUri  ) );
 		}
-	
+
 		/**
 		*	DO IT! ;)
 		*	That means:
@@ -73,6 +52,20 @@
 		*/
 		public static function run($arguments)
 		{
+			date_default_timezone_set('Europe/Zurich');
+
+			define('HTDOCS_ROOT', APP_ROOT . 'webapp/htdocs/');
+			define('SWISDK_ROOT', APP_ROOT . 'swisdk/');
+			define('SMARTY_ROOT', SWISDK_ROOT . 'lib/smarty/');
+			define('MODULE_ROOT', SWISDK_ROOT . 'modules/');
+			define('WEBAPP_ROOT', APP_ROOT.'webapp/');
+			define('LOG_ROOT', APP_ROOT.'log/');
+			define('CONTENT_ROOT' , WEBAPP_ROOT . 'content/');
+			define('CACHE_ROOT', WEBAPP_ROOT.'cache/');
+
+			require_once SWISDK_ROOT . 'core/inc.functions.php';
+			require_once SWISDK_ROOT . 'core/inc.error.php';
+
 			SwisdkError::setup();
 			Swisdk::read_configfile();
 			require_once SWISDK_ROOT . "dispatcher/inc.dispatcher.php";
@@ -93,7 +86,7 @@
 						if(!is_array(Swisdk::$config[$name]))
 							Swisdk::$config[$name] = array();
 						array_unshift(Swisdk::$config[$name],
-							substr($section, $pos+1));
+							substr($section, $po1));
 					}
 					foreach($array as $key => $value)
 						Swisdk::$config[$section.'.'.$key] = $value;
@@ -136,19 +129,11 @@
 			}
 		}
 
-		protected static $arguments;
-
-		public static function arguments()
-		{
-			return Swisdk::$arguments;
-		}
-
 		public static function register($class)
 		{
 			Swisdk::set_config_value('runtime.controller.class', $class);
 		}
-	
-	
+
 		/**
 		*	Load a "module" (actually a module is just a php-file with a class inside).
 		*	A module can exist in the swisdk-dir or the content dir. The parameter
@@ -156,59 +141,56 @@
 		*/
 		public static function load_module( $class , $dir , $instance = true )
 		{
-			if( class_exists( $class ) )
-			{
-				if( $instance )
+			if(class_exists($class)) {
+				if($instance)
 					return new $class;
-			
 				return true;
 			}
-		
+
 			$filenotfound = false;
-		
+
 			// the file name is inc.classname_lowercase.php
 			$file = "inc." . strtolower( $class ) . ".php";
-		
+
 			// now try to include the file in the dir unter swisdk
 			$swisdkpath = SWISDK_ROOT . $dir . "/" . $file;
-		
-			if( file_exists( $swisdkpath ) )
-			{
+
+			if(file_exists($swisdkpath)) {
 				require_once $swisdkpath;
-			
+
 				if( $instance )
 					return Swisdk::module_instance( $class );
 				else
 					return true;
 			}
-		
-		
+
 			// now search under content
 			$path = CONTENT_ROOT . $dir . "/" . $file;
-			if( file_exists( $path ) )
-			{
+			if(file_exists($path)) {
 				require_once $path;
-			
+
 				if( $instance )
 					return Swisdk::module_instance( $class );
 				else
 					return true;
 			}
-		
+
 			// ok we just didnt find a file in the include path... return
 			// error and say goodbye! :(
-		
-			return new FileNotFoundError( "Could not load the module $class! Could not find the include-file and the class does not exist!", $dir );
+
+			return new FileNotFoundError("Could not load the module $class!"
+				." Could not find the include-file and the class does"
+				." not exist!", $dir);
 		}
-	
+
 		public static function module_instance( $class )
 		{
-			if( class_exists( $class ) )
-			{
+			if(class_exists($class))
 				return new $class;
-			} else {
-				return CouldNotInstanceClassError( "Could no load the module $class! Class doesnt exist!" , $class  );
-			}
+			else
+				return CouldNotInstanceClassError(
+					"Could no load the module $class!"
+					." Class doesnt exist!", $class);
 		}
 	}
 
