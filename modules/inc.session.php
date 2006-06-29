3610bbda63a5cebb7c5c079c3c2e258b19754fc5
Matthias Kestenholz <matthias@spinlock.ch>
2006-06-29 20:20:07 +0200

    core.paranoia - configurable SessionHandler
    Signed-off-by: Matthias Kestenholz <matthias@spinlock.ch>

--- modules/inc.session.php
+++ modules/inc.session.php
@@ -13,6 +13,7 @@
 
 	class SessionHandler {
 		protected $user = null;
+		protected $paranoia;
 
 		protected function __construct()
 		{
@@ -20,24 +21,31 @@
 				session_start();
 			}
 
-			$agent_hash = sha1($_SERVER['HTTP_USER_AGENT']
-				.Swisdk::config_value('core.token'));
-			
+			$this->paranoia = Swisdk::config_value('core.paranoia')?true:false;
+
+			//
+			// If paranoia is activated, also include client IP
+			//
+			// The client IP might change if the user sits behind multiple proxies.
+			//
+			$user_hash = guardToken($_SERVER['HTTP_USER_AGENT']
+				.($this->paranoia?$_SERVER['REMOTE_ADDR']:''));
+
 			if(isset($_REQUEST['login_username'])
-					&& isset($_REQUEST['login_password'])) {
+					&& isset($_REQUEST['login_password'])
+					&& !isset($_SESSION['swisdk2']['user_id'])) {
 				$user = DBObject::find('User', array(
 					'user_login=' => $_REQUEST['login_username'],
 					'user_password=' => md5($_REQUEST['login_password'])));
 				if($user) {
-					session_regenerate_id(true);
 					$_SESSION['swisdk2']['user_id'] = $user->id;
-					$_SESSION['swisdk2']['user_agent'] = $agent_hash;
+					$_SESSION['swisdk2']['user_hash'] = $user_hash;
 					$this->user = $user;
 				}
 			}
 
-			if((isset($_SESSION['swisdk2']['user_agent']) 
-					&& $_SESSION['swisdk2']['user_agent']!=$agent_hash)
+			if((isset($_SESSION['swisdk2']['user_hash']) 
+					&& $_SESSION['swisdk2']['user_hash']!=$user_hash)
 					|| isset($_REQUEST['logout'])) {
 				if(isset($_COOKIE[session_name()]))
 					setcookie(session_name(), '', time()-42000, '/');
