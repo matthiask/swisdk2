0cda9ead330845cc5a988bb6689949a319863f8f
Matthias Kestenholz <matthias@spinlock.ch>
2006-09-04 00:59:18 +0200

    rename Broadcaster member functions to avoid clashes
    Signed-off-by: Matthias Kestenholz <matthias@spinlock.ch>

--- modules/inc.event-broadcaster.php
+++ modules/inc.event-broadcaster.php
@@ -6,18 +6,19 @@
 	*/
 
 	class Broadcaster {
-		public function add()
+		public function listener_add()
 		{
 			$args = func_get_args();
 			$action = array_shift($args);
 			$callback = array_shift($args);
+			array_unshift($args, $this);
 
 			$this->listeners[$action][] = array(
 				'callback' => $callback,
 				'args' => $args);
 		}
 
-		public function remove($action, $callback)
+		public function listener_remove($action, $callback)
 		{
 			if(!isset($this->listeners[$action]))
 				return;
@@ -26,7 +27,7 @@
 					unset($this->listeners[$action][$key]);
 		}
 
-		public function call($action)
+		public function listener_call($action)
 		{
 			if(!isset($this->listeners[$action]))
 				return;
@@ -34,7 +35,7 @@
 				call_user_func_array($l['callback'], $l['args']);
 		}
 
-		public function clear($action)
+		public function listener_clear($action)
 		{
 			if(isset($this->listeners[$action]))
 				unset($this->listeners[$action]);
