456d4393cd334272afd64b452898bae751077be8
Matthias Kestenholz <matthias@spinlock.ch>
2006-09-17 19:04:43 +0200

    Fix LanguageDispatcher
    Signed-off-by: Matthias Kestenholz <matthias@spinlock.ch>

--- dispatcher/inc.languagedispatcher.php
+++ dispatcher/inc.languagedispatcher.php
@@ -9,7 +9,8 @@
 		public function collect_informations()
 		{
 			$tokens = explode('/', substr($this->input(), 1));
-			if(isset($tokens[0]) && ($id = Swisdk::language($tokens[0])))
+			if(isset($tokens[0]) && $tokens[0]
+					&& ($id = Swisdk::language($tokens[0])))
 				Swisdk::set_language($tokens[0]);
 		}
 
