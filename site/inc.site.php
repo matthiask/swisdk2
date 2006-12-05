9d944e75a9561b9ab6989b5180003d42b8a1ba95
Matthias Kestenholz <matthias@spinlock.ch>
2006-12-05 14:26:09 +0100

    Do not pass smarty by reference to run_website_components
    Signed-off-by: Matthias Kestenholz <matthias@spinlock.ch>

--- site/inc.site.php
+++ site/inc.site.php
@@ -18,7 +18,7 @@
 			return Swisdk::config_value('runtime.controller.url');
 		}
 
-		public function run_website_components(&$smarty)
+		public function run_website_components($smarty)
 		{
 			$components = explode(',', Swisdk::website_config_value('components'));
 
