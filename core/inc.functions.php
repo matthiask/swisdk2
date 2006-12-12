9402ca8d504928e99510abfba4df86481e0c1653
Matthias Kestenholz <matthias@spinlock.ch>
2006-12-13 00:30:06 +0100

    randomKeys strictness fix
    Signed-off-by: Matthias Kestenholz <matthias@spinlock.ch>

--- core/inc.functions.php
+++ core/inc.functions.php
@@ -101,9 +101,10 @@
 	*/
 	function randomKeys($length)
   	{
+		$key = '';
 		$pattern = '1234567890abcdefghijklmnopqrstuvwxyz';
 		for($i=0; $i<$length; $i++)
-		     $key .= $pattern{rand(0,35)};
+			$key .= $pattern{rand(0,35)};
 		return $key;
 	}
 
