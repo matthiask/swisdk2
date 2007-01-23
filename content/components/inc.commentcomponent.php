45d83b3db037bd875bd8b192b914aa194f3c8e7c
Matthias Kestenholz <matthias@spinlock.ch>
2007-01-23 22:50:59 +0100

    CommentComponent: Do not show name, email and url field if user is already authenticated
    Signed-off-by: Matthias Kestenholz <matthias@spinlock.ch>

--- content/components/inc.commentcomponent.php
+++ content/components/inc.commentcomponent.php
@@ -32,33 +32,45 @@
 		public function run()
 		{
 			$smarty = new SwisdkSmarty();
+			$user = SessionHandler::user();
 
 			$dbo = DBObject::create('Comment');
 			$dbo->realm = $this->realm;
 			$dbo->user_id = SessionHandler::user()->id();
 			$form = new Form($dbo);
 			$form->set_title('Post a comment!');
-			$author = $form->add_auto('author');
-			$email = $form->add_auto('author_email');
-			$url = $form->add_auto('author_url');
+			if($user->id==SWISDK2_VISITOR) {
+				$author = $form->add_auto('author');
+				$email = $form->add_auto('author_email');
+				$url = $form->add_auto('author_url');
+
+				$author->set_title('Your Name');
+				$email->set_title('Your Email<br /><small>(will not be shown)</small>');
+				$url->set_title('Your Website');
+
+				$author->add_rule(new RequiredRule());
+
+				$email->add_rule(new EmailRule());
+				$email->add_rule(new RequiredRule());
+
+				$url->add_rule(new UrlRule());
+			} else {
+				$author = $form->add('comment_author', new HiddenInput());
+				$email = $form->add('comment_author_email', new HiddenInput());
+				$url = $form->add('comment_author_url', new HiddenInput());
+
+				$email->set_value($user->email);
+				$author->set_value($user->forename.' '.$user->name);
+				$url->set_value($user->url);
+			}
+
 			$text = $form->add_auto('text');
 			$form->add(new SubmitButton());
 
-			$author->set_title('Your Name');
-			$email->set_title('Your Email<br /><small>(will not be shown)</small>');
-			$url->set_title('Your Website');
 			$text->set_title('Comment');
-
+			$text->add_rule(new RequiredRule());
 			$text->set_attributes(array('style'
 				=> 'width:300px;height:250px'));
-
-			$author->add_rule(new RequiredRule());
-			$text->add_rule(new RequiredRule());
-
-			$email->add_rule(new EmailRule());
-			$email->add_rule(new RequiredRule());
-
-			$url->add_rule(new UrlRule());
 
 			if($form->is_valid() && !empty($_SERVER['HTTP_USER_AGENT'])) {
 				$dbo->realm = $this->realm;
@@ -69,12 +81,22 @@
 				$dbo->text = nl2br(strip_tags($dbo->text));
 				$dbo->store();
 				$smarty->assign('commentform', '<p>Thanks!</p>');
-			} else {
 
-				$user = SessionHandler::user();
-				if($user->id!=SWISDK2_VISITOR) {
-					$email->set_value($user->email);
-					$author->set_value($user->forename.' '.$user->name);
+				if($user->id==SWISDK2_VISITOR) {
+					setcookie('swisdk2_comment_author',
+						$dbo->author."\n"
+						.$dbo->author_email."\n"
+						.$dbo->author_url,
+						time()+90*86400);
+				}
+			} else {
+				if($user->id==SWISDK2_VISITOR && isset($_COOKIE['swisdk2_comment_author'])) {
+					$comment_author = explode("\n", $_COOKIE['swisdk2_comment_author']);
+					if(count($comment_author)==3) {
+						$author->set_value($comment_author[0]);
+						$email->set_value($comment_author[1]);
+						$url->set_value($comment_author[2]);
+					}
 				}
 
 				$smarty->assign('commentform', $form->html());
