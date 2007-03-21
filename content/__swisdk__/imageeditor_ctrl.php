17328f4996a31ba8fa4dbb1bd858c7d841560701
Matthias Kestenholz <matthias@spinlock.ch>
2007-03-21 11:51:16 +0100

    Added ImageEditor site
    Signed-off-by: Matthias Kestenholz <matthias@spinlock.ch>

---
+++ content/__swisdk__/imageeditor_ctrl.php
@@ -0,0 +1,134 @@
+<?php
+	/*
+	*	Copyright (c) 2006, Matthias Kestenholz <mk@spinlock.ch>
+	*	Distributed under the GNU General Public License.
+	*	Read the entire license text here: http://www.gnu.org/licenses/gpl.html
+	*/
+
+	require_once SWISDK_ROOT.'site/inc.site.php';
+	require_once MODULE_ROOT.'inc.smarty.php';
+	require_once MODULE_ROOT.'inc.form.php';
+	require_once MODULE_ROOT.'inc.tableview.php';
+
+	require_once SWISDK_ROOT.'content/inc.imagemanager.php';
+
+	class ImageEditorSite extends Site {
+		public function run()
+		{
+			// Image editor invocation starts here
+			$args = Swisdk::config_value('runtime.arguments');
+			if(!isset($args[0])
+					|| !isset($_SESSION['swisdk2']['image_editor'][$args[0]]))
+				SwisdkError::handle(new FatalError('Illegal ImageEditor invocation'));
+			extract($_SESSION['swisdk2']['image_editor'][$args[0]]);
+			// Image editor invocation ends here
+
+
+			$source_filename = DATA_ROOT.$htdocs_data_dir.'/'.$file;
+
+			$type = getInput('type');
+			$this_url = Swisdk::config_value('runtime.controller.url').$args[0];
+			$this_url_type = $this_url.($type?'?type='.$type.'&amp;':'?');
+
+			$image_files = ImageManager::filename($file, $types);
+
+			$work_types = $types;
+			if($type)
+				$work_types = array($type);
+
+			if($cmd = getInput('cmd')) {
+				$base = HTDOCS_DATA_ROOT.$htdocs_data_dir.'/';
+				switch($cmd) {
+					case 'restart':
+						foreach($work_types as $t)
+							unlink($base.$image->{'filename_'.$t});
+						break;
+					case 'rotate_clockwise':
+						foreach($work_types as $t)
+							ImageManager::process_commands(
+								$base.$image_files[$t], array(
+								array('rotate', 90)));
+						break;
+					case 'rotate_anticlockwise':
+						foreach($work_types as $t)
+							ImageManager::process_commands(
+								$base.$image_files[$t], array(
+								array('rotate', -90)));
+						break;
+					case 'grayscale':
+						foreach($work_types as $t)
+							ImageManager::process_commands(
+								$base.$image_files[$t], array(
+								array('grayscale')));
+						break;
+					case 'colorize-red':
+						foreach($work_types as $t)
+							ImageManager::process_commands(
+								$base.$image_files[$t], array(
+								array('colorize', '#f00', 5)));
+						break;
+					case 'colorize-green':
+						foreach($work_types as $t)
+							ImageManager::process_commands(
+								$base.$image_files[$t], array(
+								array('colorize', '#0f0', 5)));
+						break;
+					case 'colorize-blue':
+						foreach($work_types as $t)
+							ImageManager::process_commands(
+								$base.$image_files[$t], array(
+								array('colorize', '#00f', 5)));
+						break;
+					case 'crop':
+						$file = ImageManager::filename($file, 'crop');
+						$image_files['crop'] = $file;
+						if(($w = getInput('w'))
+								&& ($h = getInput('h'))
+								&& ($x = getInput('x'))
+								&& ($y = getInput('y'))) {
+							$file = $base.$file;
+							$s = getimagesize($file);
+							$ss = getimagesize($source_filename);
+							$types_desc = ImageManager::types();
+							foreach($work_types as $t) {
+								$fname = $base.$image_files[$t];
+								copy($source_filename, $fname);
+								ImageManager::process_commands($fname, array(
+									array('crop',
+										1.0*$ss[0]*$w/$s[0],
+										1.0*$ss[1]*$h/$s[1],
+										1.0*$ss[0]*$x/$s[0],
+										1.0*$ss[1]*$y/$s[1])));
+								ImageManager::process_commands($fname,
+									$types_desc[$t]['commands']);
+							}
+
+							redirect($this_url_type);
+						} else {
+							ImageManager::register_type('crop', array(
+								'commands' => array(
+									array('scale_bounding_box', 600, 600))));
+							ImageManager::generate_type($source_filename, 'crop',
+								$htdocs_data_dir);
+						}
+						break;
+				}
+			}
+
+			ImageManager::generate_types($source_filename,
+				$htdocs_data_dir);
+
+			$smarty = new SwisdkSmarty();
+			$smarty->assign('htdocs_data_dir', $htdocs_data_dir);
+			$smarty->assign('images', $image_files);
+			$smarty->assign('types', $types);
+			$smarty->assign('type', $type?$type:'full');
+			$smarty->assign('this_url', $this_url);
+			$smarty->assign('this_url_type', $this_url_type);
+			$smarty->display_template('base.imageeditor');
+		}
+	}
+
+	Swisdk::register('ImageEditorSite');
+
+?>
---
+++ content/base/imageeditor.tpl
@@ -0,0 +1,188 @@
+{extends file="base.admin"}
+
+{block name="head"}
+{literal}
+<style type="text/css">
+.s-table tbody tr:hover {
+	background: #cfc7b7;
+	cursor: pointer;
+}
+
+#typechooser {
+	width: 300px;
+	float: left;
+}
+
+#typechooser h2 {
+	margin: 0;
+	padding: 5px;
+	background: #dfd8c7;
+}
+
+#typechooser ul {
+	height: 670px;
+	overflow: scroll;
+	margin: 0;
+	padding: 0;
+}
+
+#typechooser li {
+	list-style: none;
+	margin: 0;
+	padding: 10px 0;
+	text-align: center;
+}
+
+#typechooser li span {
+	display: block;
+}
+
+#typechooser li a {
+	display: block;
+	margin: 0 auto;
+	width: 240px;
+	padding: 10px;
+	background: #dfd8c7;
+	color: #000;
+	text-decoration: none;
+	overflow: hidden;
+}
+
+#typechooser li a:hover {
+	background: #efe8d7;
+}
+
+#editarea {
+}
+
+#editarea #tools {
+	background: #dfd8c7;
+	height: 32px;
+}
+
+#editarea #tools a {
+	display: block;
+	float: left;
+	padding: 5px 7px;
+	border: 1px solid #dfd8c7;
+	background: #efe8d7;
+}
+
+#editarea #tools a:hover {
+	background: #dfd8c7;
+}
+
+#imagearea {
+	position: relative;
+	margin-left: 300px;
+}
+
+</style>
+{/literal}
+{/block}
+
+{block name="body"}
+<div id="typechooser">
+	<h2>Act on:</h2>
+	<ul>
+		<li>
+			<a href="{$this_url}">
+			All types
+			</a>
+		</li>
+		{foreach from=$types item=t}
+		<li>
+			<a href="{$this_url}?type={$t}">
+				<img src="{webroot key=data}/{$htdocs_data_dir}/{$images.$t}?{''|uniqid}" />
+				<span>{$t}</span>
+			</a>
+		</li>
+		{/foreach}
+	</ul>
+</div>
+
+
+<div id="editarea">
+	<div id="tools">
+		<a href="{$this_url_type}cmd=restart"
+				title="Start over">
+			<img src="/media/img/icons/star.png" />
+		</a>
+		<a href="{$this_url_type}cmd=rotate_clockwise"
+				title="Rotate clockwise">
+			<img src="/media/img/icons/shape_rotate_clockwise.png" />
+		</a>
+		<a href="{$this_url_type}cmd=rotate_anticlockwise"
+				title="Rotate anti-clockwise">
+			<img src="/media/img/icons/shape_rotate_anticlockwise.png" />
+		</a>
+		<a href="{$this_url_type}cmd=grayscale"
+				title="Convert to grayscale">
+			grayscale
+		</a>
+		<a href="{$this_url_type}cmd=colorize-red"
+				title="Colorize image with red color">
+			colorize red
+		</a>
+		<a href="{$this_url_type}cmd=colorize-green"
+				title="Colorize image with green color">
+			colorize green
+		</a>
+		<a href="{$this_url_type}cmd=colorize-blue"
+				title="Colorize image with blue color">
+			colorize blue
+		</a>
+		<a href="{$this_url_type}cmd=crop"
+				title="Crop image">
+			<img src="/media/img/icons/shape_handles.png" />
+		</a>
+	</div>
+
+	<div id="imagearea">
+
+		{if $smarty.get.cmd=='crop'}
+<script type="text/javascript" src="{webroot key=js}/scriptaculous/prototype.js"></script>
+<script type="text/javascript" src="{webroot key=js}/scriptaculous/scriptaculous.js"></script>
+<script type="text/javascript" src="{webroot key=js}/cropper/cropper.js"></script>
+
+
+<form action="{$smarty.server.REQUEST_URI}" method="post">
+	<input type="hidden" name="w" id="w" />
+	<input type="hidden" name="h" id="h" />
+	<input type="hidden" name="x" id="x" />
+	<input type="hidden" name="y" id="y" />
+	<input type="submit" value="crop" />
+</form>
+
+<img src="{webroot key=data}/{$htdocs_data_dir}/{$images.crop}?{''|uniqid}" id="image" />
+
+<script type="text/javascript">
+//<![CDATA[
+{literal}
+	Event.observe(window, 'load', function(){
+		new Cropper.Img('image', {onEndCrop: onEndCrop});
+	});
+
+	function onEndCrop(coords, dimensions) {
+		$('w').value = dimensions.width;
+		$('h').value = dimensions.height;
+		$('x').value = coords.x1;
+		$('y').value = coords.y1;
+	}
+{/literal}
+//]]>
+</script>
+
+
+		{else}
+		<table style="width:620px;height:620px;border:1px solid #aba;">
+			<tr><td style="text-align:center;vertical-align:middle">
+			<img src="{webroot key=data}/{$htdocs_data_dir}/{$images.$type}?{''|uniqid}" />
+			</td></tr>
+		</table>
+		{/if}
+	</div>
+</div>
+
+<br style="clear:both" />
+{/block}
