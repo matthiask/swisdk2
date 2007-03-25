00d8df682a772fd0ac6a626e87b2e241e98a3165
Matthias Kestenholz <matthias@spinlock.ch>
2007-03-25 17:02:57 +0200

    Updated Smarty to version 2.6.18
    Signed-off-by: Matthias Kestenholz <matthias@spinlock.ch>

--- lib/smarty/libs/Smarty.class.php
+++ lib/smarty/libs/Smarty.class.php
@@ -27,10 +27,10 @@
  * @author Monte Ohrt <monte at ohrt dot com>
  * @author Andrei Zmievski <andrei@php.net>
  * @package Smarty
- * @version 2.6.16
+ * @version 2.6.18
  */
 
-/* $Id: Smarty.class.php,v 1.526 2006/11/30 17:01:28 mohrt Exp $ */
+/* $Id: Smarty.class.php,v 1.528 2007/03/06 10:40:06 messju Exp $ */
 
 /**
  * DIR_SEP isn't used anymore, but third party apps might
@@ -464,7 +464,7 @@ class Smarty
      *
      * @var string
      */
-    var $_version              = '2.6.16';
+    var $_version              = '2.6.18';
 
     /**
      * current template inclusion depth
