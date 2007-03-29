8eac4507098b14231b0ba1383f317798ac338605
Matthias Kestenholz <matthias@spinlock.ch>
2007-03-29 19:40:27 +0200

    Added ImageTableViewColumn
    Signed-off-by: Matthias Kestenholz <matthias@spinlock.ch>

---
+++ content/inc.imagetableview.php
@@ -0,0 +1,27 @@
+<?php
+	/*
+	*	Copyright (c) 2007, Matthias Kestenholz
+	*	Distributed under the GNU General Public License
+	*	Read the entire license text here: http://www.gnu.org/licenses/gpl.html
+	*/
+
+	require_once MODULE_ROOT.'inc.tableview.php';
+	require_once SWISDK_ROOT.'content/inc.imagemanager.php';
+
+	class ImageTableViewColumn extends TableViewColumn {
+		public function html(&$data)
+		{
+			$type = $this->args[0];
+			$path = $this->args[1];
+			$fname = $data[$this->column];
+
+			return '<img src="'
+				.Swisdk::config_value('runtime.webroot.data', '/data')
+				.'/'.$path.'/'
+				.ImageManager::generate_type(
+					DATA_ROOT.$path.'/'.$fname, $type, $path)
+				.'" />';
+		}
+	}
+
+?>
